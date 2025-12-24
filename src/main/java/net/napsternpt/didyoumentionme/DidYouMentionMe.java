4900c608bf964aad5d3b81e9cd11b1718522dcff
NapsterNPT <188631398+napsternpt@users.noreply.github.com>
2025-12-24 11:12:16 +0100

    first upload

---
+++ src/main/java/net/napsternpt/didyoumentionme/DidYouMentionMe.java
@@ -0,0 +1,16 @@
+package net.napsternpt.didyoumentionme;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DidYouMentionMe implements ModInitializer {
+
+	public static final String MOD_ID = "didyoumentionme";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/napsternpt/didyoumentionme/DidYouMentionMeDataGenerator.java
@@ -0,0 +1,11 @@
+package net.napsternpt.didyoumentionme;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DidYouMentionMeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
